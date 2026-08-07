8e86533547b27cb3d03d425908202db8403b8093
Sharl RAJAN <sharlr@live.com>
2026-08-08 01:16:40 +0300

    Delete src directory

--- src/i18n/index.ts
+++
@@ -1,4 +0,0 @@
-export { LanguageProvider, useLanguage } from "./LanguageContext";
-export { LANGUAGES, DEFAULT_LANGUAGE, LANGUAGE_LABELS, RTL_LANGUAGES } from "./config";
-export { getTranslation, loadTranslation } from "./utils";
-export type { Language } from "./config";
